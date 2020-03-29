68cedc70c22599c2e9f95b3529354388ab8f8535
Hhumour <you@example.com>
2020-03-29 17:27:58 +0530

    Module 2 task completed

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            var customers = GetCustomers();
+            return View(customers);
+        }
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IActionResult HttpNotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+
         }
     }
-}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,55 +10,21 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/ Random
-        public IActionResult Random()// ActionResult has many subtypes i.e. ViewResult, PartialViewResult, ContentResult, etc.
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name="Customer 1"},
-                new Customer {Name="Customer 2"}
-            };
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-            //var viewResult = new ViewResult();
-            //viewResult.ViewData.Model
-            /*return Content("Hello World");
-            return HttpNotFound();
-            return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });*/
-            return View(viewModel);
 
-        }
-        
-        
-        /*[Route("movies/released/{year}/{month}")]//[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public IActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-                
-        }
-
-        // GET: Movies/Edit/1   OR    GET:Movies/Edit?id=1
-        public IActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
         //GET: Movies
-        public IActionResult Index(int? pageIndex, string sortBy) // '?' is used to make it nullable, But string is by default nullable
+        public ViewResult Index() // '?' is used to make it nullable, But string is by default nullable
         {
-            if (!pageIndex.HasValue)
+            var movies = GetMovies();
+            return View(movies); 
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
             {
-                pageIndex = 1;
-            }
-            if (String.IsNullOrWhiteSpace(sortBy)){
-                sortBy = "Name";
-            }
-            return Content(String.Format("pageIndex={0}&&sortBy={1}", pageIndex, sortBy));
-        }*/
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
 
     }
 }
